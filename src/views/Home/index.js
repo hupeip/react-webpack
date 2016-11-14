0c7b823c3ce57a914eca5d9b297917deec4bbb32
hupeip <hupei_323@163.com>
2016-11-14 19:59:44 +0800

    生命周期

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,43 @@
 import React, { Component, PropTypes } from 'react'
 import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters  } from '../../redux/actions/action'
 import { connect } from 'react-redux'
+import { findDOMNode } from 'react-dom'
 import NavBar from '../navBar'
 import Header from '../Header'
 import Footer from '../Footer'
 
 class Home extends Component {
-
+    //初始化，在extends Component中getInitailState不能使用
+    constructor (props) {
+        //初始化时，传递props给React.Component
+        super(props)
+        //this.handleClick不会自动绑定到react实例上，需要.bind(this)才能绑定
+        //可以在构造器中绑定，也可以在<button onClick={this.handleClick.bind(this)}>click</button>中绑定
+        this.handleClick = this.handleClick.bind(this)
+        this.state = {
+            aaa: 'hello'
+        }
+    }
+    componentWillMount () {
+        //在初始化渲染之前执行
+        console.log('初始化渲染之前执行')
+    }
+    componentDidMount () {
+        //在初始化渲染之后调用一次
+    }
+    shouldComponentUpdate (nextProps, nextState) {
+        alert(nextState.aaa)
+        return true
+    }
+    handleClick () {
+        this.setState({
+            aaa: 'hello world'
+        })
+        // alert(this.state.aaa)
+    }
+    handleFocus () {
+        console.log((findDOMNode(this.refs.myText)).value)
+    }
     render () {
         const { dispatch, visibleTodos, visibilityFilter } = this.props
         return (
@@ -25,6 +56,11 @@ class Home extends Component {
                     onFilterChange = {nextFilter =>
                         dispatch(setVisibilityFilter(nextFilter))}
                 />
+                <button onClick={this.handleClick}>click</button>
+                <p>{this.state.aaa}</p>
+                {this.props.name}
+                <input type="text" ref="myText"/>
+                <button onClick={this.handleFocus.bind(this)}>focus on text</button>
                 {this.props.children || "Welcome to your Inbox"}
             </div>
         )
@@ -40,7 +76,13 @@ Home.propTypes = {
         'SHOW_ALL',
         'SHOW_COMPLETED',
         'SHOW_ACTIVE'
-    ]).isRequired
+    ]).isRequired,
+    name: PropTypes.string
+}
+
+//默认属性
+Home.defaultProps = {
+    name: 'hello world!!!'
 }
 
 function selectTodos(todos, filter) {
@@ -54,7 +96,7 @@ function selectTodos(todos, filter) {
     }
 }
 
-function select (state,ower) {ower.name
+function select (state,ower) {
     return {
         visibleTodos: state.todos,//selectTodos(state.todos, state.visibilityFilter),
         visibilityFilter: state.visibilityFilter
